b81d7c0e780e2e8f8184e4c2282e18e7f1a01698
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2019-05-31 16:08:47 +0200

    refactored script, restored all current available spark versions

--- mini-cross-build.pl
+++ mini-cross-build.pl
@@ -2,17 +2,21 @@
 
 #!/usr/bin/perl
 
-use File::Slurp;
 use strict;
 use warnings;
 
-my @spark_versions = ("2.1.0");
-# ("2.0.0", "2.0.1", "2.0.2", "2.1.0", "2.1.1", "2.1.2", "2.1.3", "2.2.0", "2.2.1", "2.2.2", "2.3.0", "2.3.1", "2.3.2", "2.4.0");
+my @spark_versions = (
+    "1.6.0", "1.6.1", "1.6.2", "1.6.3",
+    "2.0.0", "2.0.1", "2.0.2", "2.1.0",
+    "2.1.1", "2.1.2", "2.1.3",
+    "2.2.0", "2.2.1", "2.2.2", "2.2.3",
+    "2.3.0", "2.3.1", "2.3.2", "2.3.3",
+    "2.4.0", "2.4.1", "2.4.2", "2.4.3");
 
 foreach my $spark_version (@spark_versions) {
     print "Next spark version ".$spark_version;
     print "\nbuilding\n";
-    print "\nGoing to run: ./sbt/sbt  -DsparkVersion=$spark_version clean +publishSigned\n";
-    print `./sbt/sbt  -DsparkVersion=$spark_version clean +publishSigned`;
+    print "\nGoing to run: ./sbt/sbt -DsparkVersion=$spark_version clean +publishSigned\n";
+    print `./sbt/sbt -DsparkVersion=$spark_version clean +publishSigned`;
     print "\nbuilt\n";
 }
